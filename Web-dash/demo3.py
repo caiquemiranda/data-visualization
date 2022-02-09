9ab70d375a05f26efbf5aa65d36b956257464d3e
caiquemiranda <caiquemirandas@gmail.com>
2022-02-09 09:42:00 -0300

    web dash fist step

--- Web-dash/demo3.py
+++ Web-dash/demo3.py
@@ -1,12 +1,25 @@
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+
+tips = px.data.tips()
+col_options = [dict(label=x, value=x) for x in tips.columns]
 
 app =dash.Dash(__name__)
 
 app.layout = html.Div(children= [
     html.H1('Demo: Plotly Express in dash with tips dataset'),
-    html.H2('"I"m a sub-header'),
+    dcc.Dropdown(id='x', options=col_options),
+    dcc.Graph(id='graph', figure=px.scatter(tips, x='total_bill', y='tip')),
 
 ])
+
+@app.callback(Output('graph','figure'), [Input('x', 'value')])
+
+def cb(x):
+    return px.scatter(tips, x=x)
 
 app.run_server(debug=True)
